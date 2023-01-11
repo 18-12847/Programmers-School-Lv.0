ddd0381b500e2d18ed55c2d3df534282d4384da0
18-12847 <jae2933@naver.com>
2023-01-11 10:58:14 +0900

    가위 바위 보 소스 코드

---
+++ 가위 바위 보.py
@@ -0,0 +1,10 @@
+def solution(rsp):
+    rsp = list(rsp)
+    for i in range(len(rsp)):
+        if rsp[i] == "2":
+            rsp[i] = "0"
+        elif rsp[i] == "0":
+            rsp[i] = "5"
+        elif rsp[i] == "5":
+            rsp[i] = "2"
+    return "".join(rsp)
